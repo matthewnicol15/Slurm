91238b98a39b162a6e4b9c2be4201c0b2d1a51c9
mathewnicol15 <matthew.nicol@york.ac.uk>
2022-02-14 15:41:31 +0000

    Added in polarity as an input, automatically selects the fiducial cuts in the analysis code based on this

--- FTOF/FTOF_process_make.sh
+++ FTOF/FTOF_process_make.sh
@@ -11,7 +11,7 @@ rm process_FTOF"$NUMBER".sh
 touch process_FTOF"$NUMBER".sh
 
 echo 'gROOT->ProcessLine(".L ~/Documents/Macros/FTOF_Efficiency_Studies/FTOF_Unified_2D.C++");' >> process_FTOF"$NUMBER".sh
-echo 'gROOT->ProcessLine("FTOF_Unified_2D(\"/cache/clas12/rg-a/production/recon/fall2018/torus+1/pass1/v0/dst/recon/00'$NUMBER'/*.hipo\",\"/volatile/clas12/matthewn/FTOF/RGA_Fall18_Outbending/RGA_Fall18_Outbending_'$NUMBER'.root\")");' >> process_FTOF"$NUMBER".sh
+echo 'gROOT->ProcessLine("FTOF_Unified_2D(\"/cache/clas12/rg-a/production/recon/fall2018/torus+1/pass1/v0/dst/recon/00'$NUMBER'/*.hipo\",\"/volatile/clas12/matthewn/FTOF/RGA_Fall18_Outbending/RGA_Fall18_Outbending_'$NUMBER'.root\",\"Outbending\")");' >> process_FTOF"$NUMBER".sh
 
 echo "Made process_FTOF"$NUMBER".sh"
 
